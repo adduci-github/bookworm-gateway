4db41a1a6f9f140933f82752ab7a12dac8a11324
GFZ\jyj94 <jyj94@golfzon.com>
2024-01-09 10:44:54 +0900

    최초 파일 Push

---
+++ bookworm-gateway/src/test/java/com/bookworm/bookwormgateway/BookwormGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworm.bookwormgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
